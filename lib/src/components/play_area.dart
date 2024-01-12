6a4b114b7bfa6959132f443b3fa612036022dbd2
Jorge Coca <jorge.coca.github@gmail.com>
2024-01-12 16:13:47 -0600

    feat: create game

---
+++ lib/src/components/play_area.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import '../brick_breaker.dart';
+
+class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
+  PlayArea() : super(paint: Paint()..color = const Color(0xfff2e8cf));
+
+  @override
+  FutureOr<void> onLoad() {
+    super.onLoad();
+    size = Vector2(game.width, game.height);
+  }
+}
